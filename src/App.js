3d4f1e532a067e70aab99187ea181d877a9bf18d
Garbuzenko <mike.garbuzenko@gmail.com>
2023-11-06 18:21:43 +0300

    ok

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ServiceAddForm from "./components/ServiceAddForm";
+import ServiceList from "./components/ServiceList";
+
+function App() {
+  return (
+    <div className="App">
+      <ServiceAddForm />
+      <ServiceList />
+    </div>
+  );
+}
+
+export default App;
